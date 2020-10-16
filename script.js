eb3cf524db44aa270f4247e9c2475e40c79ba555
ChaplinGleb <ChaplinGleb@users.noreply.github.com>
2020-10-16 04:47:32 +0300

    new input (without historu at all)

--- script.js
+++ script.js
@@ -1,60 +1,33 @@
-const resultCalc = document.getElementById("box_result")
+const resultHistory = document.getElementById("result-history")
+const resultResult = document.getElementById("result-result")
 const historyOutput = document.getElementById("history")
-let i = 0
-function input(i){
-   if (resultCalc.value.length > 11){
-      resultCalc.value = "Too much";
+let j = 0
+
+function inputNumber(i){
+   if (j != 0){
+      resultResult.value = ""
+      j = 0
    }
-   if (resultCalc.value != "Too much" && resultCalc.value != "Error"){
-      resultCalc.value += i;   
-   }
-   if(resultCalc.value.substr(resultCalc.value.length - 2) == "**" || resultCalc.value.substr(resultCalc.value.length - 2) == "//" || resultCalc.value.substr(resultCalc.value.length - 2) == "--" || resultCalc.value.substr(resultCalc.value.length - 2) == "++"){
-      resultCalc.value = resultCalc.value.substring(0, resultCalc.value.length - 1);
-   }
-}
-function result(){
-   let item, p
-   if (eval(resultCalc.value) == undefined){
-      resultCalc.value = "Error"
-      return
-   }
-   if (resultCalc.value == "Error"){
-      return
-   }
-   if (eval(resultCalc.value) == Infinity){
-      resultCalc.value = "Error"
-      return
-   }
-   if (resultCalc.value != eval(resultCalc.value)){
-      item = resultCalc.value + '=' + eval(resultCalc.value)
-   }
-   resultCalc.value = eval(resultCalc.value)
-   if(item != undefined){
-      p = document.createElement('p')
-      p.className ='history-list_item'
-      p.innerHTML = item
-      if (i <= 11){
-         historyOutput.prepend(p)
-         ++i
-      }else {
-         historyOutput.removeChild(historyOutput.lastChild);
-         historyOutput.prepend(p)
-      }
-   }
-}
-function backspace(){
-   if (resultCalc.value == "Error" || resultCalc.value == "Too much"){
-      resultCalc.value = ""
-   }
-   resultCalc.value = resultCalc.value.substring(0, resultCalc.value.length - 1)
-}
-function reset(){
-   resultCalc.value = ""
+   resultResult.value += i
 }
 
-$(document).ready(function(){
-   $("#openHistory").click(function(){
-      $("div.history").toggleClass("history_left")
-         $("#openHistory").toggleClass("btn-history-rotate")
-   });
-})
+function inputSymbol(a){
+   resultHistory.value = resultResult.value + a
+   ++j
+}
+
+function result(){
+   let num, result 
+   num = resultHistory.value + resultResult.value
+   num = eval(num)
+   result = resultHistory.value + resultResult.value + "="
+   resultHistory.value = result
+   resultResult.value = ""
+   resultResult.value = num
+   ++j
+}
+function reset(){
+   resultResult.value = ""
+   resultHistory.value = ""
+   j = 0
+}
